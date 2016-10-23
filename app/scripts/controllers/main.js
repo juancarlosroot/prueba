dbd02badf64590c092e5fdee3b08d06d92bc9593
Juan Carlos Hernández <rondostrings@gmail.com>
2016-10-23 12:53:10 -0500

    ök

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pruebaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pruebaApp
+ */
+angular.module('pruebaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
